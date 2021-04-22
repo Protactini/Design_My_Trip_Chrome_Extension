31cafe5e80935beabf391b58ccb49c0c632c6a49
Hongzhi Zhu <protactini0819@gmail.com>
2021-04-22 12:42:29 -0700

    Change Button Appearance

--- content.js
+++ content.js
@@ -3,6 +3,10 @@ const findTheApproveButton = function () {
     //Find all Approve button
     const allButton = document.querySelectorAll('[aria-label="Approve"]');
 
+    //Add New .CSS
+    const styleEl = document.createElement("style");
+    const styleSheet = styleEl.sheet;
+
     //Function of added Button
     const showinfo = function () {
       const folder = this.parentNode.parentNode.parentNode.parentNode
@@ -23,17 +27,54 @@ const findTheApproveButton = function () {
           answerNode[answerNode.length - 1].firstChild.textContent
         }`
       );
-
-      //   console.log(folder);
-      //   console.log(answerNodeName);
-      //   console.log(answerNode.length);
     };
+
+    // Add rules to .css file
+    document.styleSheets[0].addRule(
+      ".getInfoButton",
+      "display: inline-block;" +
+        "background-color: #e67e22;" +
+        "border: 1px solid #e67e22;" +
+        "text-align: center;" +
+        "border-radius: 10px;" +
+        "height:37px;" +
+        "weight:200px;" +
+        "color: #fff;" +
+        "font-weight: 400;" +
+        "font-size: 125%;" +
+        "margin-top: 6px;" +
+        "text-align: center;" +
+        "transition: background-color 0.2s, board 0.2s, color 0.2s;"
+    );
+
+    document.styleSheets[0].addRule(
+      ".getInfoButton:hover,.getInfoButton:active",
+      "display: inline-block;" +
+        "background-color: #cf6d17;" +
+        "border: 1px solid #cf6d17;" +
+        "text-align: center;" +
+        "color: #000;"
+    );
 
     //Add button ans change text
     for (let i = 0; i < allButton.length; i++) {
       const getInfoButton = document.createElement("button");
       getInfoButton.id = `getinfo_${i}`;
+      getInfoButton.className = "getInfoButton";
       getInfoButton.textContent = "Get Info";
+      // getInfoButton.style.cssText =
+      //   "display: inline-block;" +
+      //   "background-color: #e67e22;" +
+      //   "border: 1px solid #e67e22;" +
+      //   "text-align: center;" +
+      //   "border-radius: 10px;" +
+      //   "height:37px;" +
+      //   "weight:200px;" +
+      //   "color: #fff;" +
+      //   "font-weight: 400;" +
+      //   "font-size: 125%;" +
+      //   "margin-top: 6px;" +
+      //   "text-align: right;";
 
       allButton[i].querySelector('[dir="auto"]').textContent =
         "Get Info and Approve";
@@ -42,8 +83,6 @@ const findTheApproveButton = function () {
         .parentNode.parentNode.prepend(getInfoButton);
 
       getInfoButton.addEventListener("click", showinfo);
-
-      //   console.log();
     }
 
     // getInfoButton.addEventListener("click");
