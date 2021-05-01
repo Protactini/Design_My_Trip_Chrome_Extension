8b75a5943b0d451e81bb884fdc16bd4a837b9a64
Hongzhi Zhu <protactini0819@gmail.com>
2021-05-01 00:04:09 -0700

    Add info need to be collected and basic file generate function
    use is new way to find answer and download info for every member

--- content.js
+++ content.js
@@ -9,10 +9,32 @@ const findTheApproveButton = function () {
 
     //Function of added Button
     const showinfo = function () {
+      console.log(this);
+
       const folder = this.parentNode.parentNode.parentNode.parentNode
         .childNodes[1].childNodes;
       const answerNode = folder[folder.length - 1].childNodes;
       const allNodes = folder[folder.length - 2].firstChild.childNodes;
+      const name = this.parentNode.parentNode.parentNode.firstChild.firstChild
+        .childNodes[1].firstChild.firstChild.firstChild.firstChild.firstChild
+        .textContent;
+      const livingArea = this.parentNode.parentNode.parentNode.parentNode.childNodes[1].querySelectorAll(
+        "span"
+      );
+      let csvString = "data:application/csv,";
+
+      //Get name
+      console.log(`Name: ${name}`);
+      csvString += `Name: ${name},`;
+
+      //Get living area
+      for (const item of livingArea) {
+        if (item.textContent.includes("Lives in")) {
+          console.log(`Lives in: ${item.childNodes[1].textContent}`);
+          csvString += `${item.childNodes[1].textContent},`;
+        }
+        // console.log(item.textContent);
+      }
 
       for (let i = 0; i < allNodes.length; i++) {
         const answerBlock = allNodes[i].childNodes;
@@ -20,6 +42,8 @@ const findTheApproveButton = function () {
         console.log(
           `Answer ${i + 1}: ${answerBlock[answerBlock.length - 1].textContent}`
         );
+
+        csvString += `${answerBlock[answerBlock.length - 1].textContent},`;
       }
 
       console.log(
@@ -27,6 +51,27 @@ const findTheApproveButton = function () {
           answerNode[answerNode.length - 1].firstChild.textContent
         }`
       );
+
+      csvString += `${
+        answerNode[answerNode.length - 1].firstChild.textContent
+      },`;
+
+      // const getInfoButton = document.createElement("button");
+      // getInfoButton.id = `downloadinfo`;
+      // getInfoButton.className = "downloadinfo";
+      // getInfoButton.textContent = "Download Info";
+
+      // this.parentNode.prepend(getInfoButton);
+      // getInfoButton.setAttribute("href", csvString);
+      // getInfoButton.setAttribute("download", "data.csv");
+      // console.log(getInfoButton);
+
+      var link = document.createElement("a");
+      link.setAttribute("href", csvString);
+      var date = new Date().getTime();
+      var filename = new Date(date).toLocaleDateString();
+      link.setAttribute("download", filename + ".csv");
+      link.click();
     };
 
     // Add rules to .css file
@@ -36,14 +81,15 @@ const findTheApproveButton = function () {
         "background-color: #e67e22;" +
         "border: 1px solid #e67e22;" +
         "text-align: center;" +
-        "border-radius: 10px;" +
-        "height:37px;" +
+        "border-radius: 7px;" +
+        "height:36px;" +
         "weight:200px;" +
         "color: #fff;" +
-        "font-weight: 400;" +
+        "font-weight: 500;" +
         "font-size: 125%;" +
         "margin-top: 6px;" +
         "text-align: center;" +
+        "padding: 0px 35px;" +
         "transition: background-color 0.2s, board 0.2s, color 0.2s;"
     );
 
@@ -76,10 +122,11 @@ const findTheApproveButton = function () {
       //   "margin-top: 6px;" +
       //   "text-align: right;";
 
-      allButton[i].querySelector('[dir="auto"]').textContent =
-        "Get Info and Approve";
+      // allButton[i].querySelector('[dir="auto"]').textContent =
+      //   "Get Info and Approve";
       allButton[i].parentNode.parentNode.prepend(getInfoButton);
 
+      // getInfoButton.showinfo();
       getInfoButton.addEventListener("click", showinfo);
     }
 
